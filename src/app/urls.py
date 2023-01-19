954fc7c84327d0a7c7691b239d729f26b8bdea09
KulykDima <dkulik912@gmail.com>
2023-01-19 23:43:32 +0200

    listcreate

--- src/app/urls.py
+++ src/app/urls.py
@@ -23,7 +23,8 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html'), name='index'),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
-    path('accounts/', include('django.contrib.auth.urls'))
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('posts/', include('post.urls')),
 ]
 
 if settings.DEBUG:
---
+++ src/post/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from django.core.exceptions import ValidationError
+
+from .models import Posts
+
+
+class CreatePostForm(forms.ModelForm):
+    class Meta:
+        model = Posts
+        fields = [
+            'title',
+            'author',
+            'text',
+        ]
