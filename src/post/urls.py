2a41c30ea0f4f7bf545dbe9adec73d7a68f47840
KulykDima <dkulik912@gmail.com>
2023-02-04 02:05:13 +0200

    bloggerslistadded

--- src/post/urls.py
+++ src/post/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from post.views import AddDislike
+from post.views import AddDislike, ListOfBloggers
 from post.views import AddLike
 from post.views import CreatePost
 from post.views import PostDetail
@@ -16,4 +16,5 @@ urlpatterns = [
     path('<int:pk>/like/', AddLike.as_view(), name='like'),
     path('<int:pk>/dislike/', AddDislike.as_view(), name='dislike'),
     path('update/<uuid:uuid>', PostUpdate.as_view(), name='update'),
+    path('bloggers/', ListOfBloggers.as_view(), name='blogger_list'),
 ]
