9e438f3358007e0f6a7387e3e72ff601a509bf16
KulykDima <dkulik912@gmail.com>
2023-03-16 20:19:55 +0200

    upd_docker_yml_nginxconf_tests_for_postgres

--- Dockerfile
+++ Dockerfile
@@ -18,7 +18,7 @@ RUN rm requirements-dev.txt
 
 COPY src .
 # COPY .env ../.env
-COPY ./dump.json .
+COPY ./src/dump.json .
 
 EXPOSE 8890
 
--- src/accounts/test/test_views.py
+++ src/accounts/test/test_views.py
@@ -66,12 +66,40 @@ class TestViews(TestCase):
 
 
 class TestLogin(TestCase):
+    test_user1 = None
+    test_user2 = None
+    test_message = None
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.test_user1 = 'user_1'
+        User.objects.create_user(
+            username=cls.test_user1,
+            password='Dimo4ka22222',
+            email='user_1@test.com',
+            first_name='Dimitriy',
+            is_activated=True
+        )
+
+        cls.test_user2 = 'user_2'
+        User.objects.create_user(
+            username=cls.test_user2,
+            password='Dimo4ka22222',
+            email='user_2@test.com',
+            first_name='user_2',
+            is_activated=True
+        )
+
+        # cls.message = 'Subject'
+        Message.objects.create(
+            sender=User.objects.get(username=cls.test_user1),
+            recipient=User.objects.get(username=cls.test_user2),
+            subject='Subject',
+            body='123'
+        )
+
     def setUp(self):
-        test_user1 = User.objects.create_user(username='user_1', password='Dimo4ka22222')
-        test_user1.save()
-        test_user2 = User.objects.create_user(username='user_2', password='Dimkaaaaa2')
-        test_user2.save()
-        self.message = Message.objects.create(sender=test_user1, recipient=test_user2, subject='none', body='123')
+        self.message = Message.objects.get(sender=User.objects.get(username=self.test_user1))
         self.client = Client()
 
     def test_logged_in_uses_correct_template(self):
@@ -87,13 +115,13 @@ class TestLogin(TestCase):
         self.assertRedirects(login_post_data, index, status_code=302, target_status_code=200)
 
     def test_if_not_logged_in(self):
-        blogger = User.objects.get(id=1)
+        blogger = User.objects.get(username=self.test_user1)
         response_1 = self.client.get(reverse('accounts:profile'))
         response_2 = self.client.get(reverse('posts:blogger_detail', kwargs={'pk': blogger.pk}))
         response_3 = self.client.get(reverse('accounts:inbox'))
 
         self.assertRedirects(response_1, '/accounts/login/?next=%2Faccounts%2Fprofile%2F')
-        self.assertRedirects(response_2, '/accounts/login/?next=%2Fposts%2Fbloggers%2Fblogger%2F1',
+        self.assertRedirects(response_2, '/accounts/login/?next=/posts/bloggers/blogger/1',
                              status_code=302,
                              target_status_code=200)
         self.assertRedirects(response_3, '/accounts/login/?next=/accounts/profile/inbox',
